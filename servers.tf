51769a8beeafb4cfdb39e7015624c7f3adf8961c
sebug <sebastian.gfeller@gmail.com>
2018-08-05 16:27:31 +0200

    Adding first server

---
+++ servers.tf
@@ -0,0 +1,27 @@
+resource "google_compute_instance" "firstserver" {
+  name = "thefirstserver"
+  machine_type = "n1-standard-1"
+  zone = "europe-west2-a"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-8"
+    }
+  }
+
+  network_interface {
+    subnetwork = "${google_compute_subnetwork.dev-subnet.name}"
+
+    access_config {
+      
+    }
+  }
+
+  metadata {
+    nirb = "nurb"
+  }
+
+  service_account {
+    scopes = ["userinfo-email", "compute-ro", "storage-ro"]
+  }
+}
