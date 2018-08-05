acebada676b6c02577efc8bff5a406111852bc3e
sebug <sebastian.gfeller@gmail.com>
2018-08-05 15:58:35 +0200

    Adding Azure

--- resources.tf
+++ resources.tf
@@ -23,3 +23,46 @@ resource "aws_subnet" "subnet2" {
   vpc_id = "${aws_vpc.environment-example-two.id}",
   "availability_zone" = "eu-central-1b"
 }
+
+resource "aws_security_group" "subnetsecurity" {
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  ingress {
+    cidr_blocks = [
+      "${aws_vpc.environment-example-two.cidr_block}"
+    ]
+
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+  }
+}
+
+resource "azurerm_resource_group" "azy_network" {
+  location = "West Europe"
+  name = "devresgrp"
+}
+
+resource "azurerm_virtual_network" "blue_virtual_network" {
+  address_space = ["10.0.0.0/16"]
+  location = "West Europe"
+  name = "bluevirtnetwork"
+  resource_group_name = "${azurerm_resource_group.azy_network.name}"
+  dns_servers = [
+    "10.0.0.4",
+    "10.0.0.5"
+  ]
+
+  subnet {
+    name = "subnet1"
+    address_prefix = "10.0.1.0/24"
+  }
+
+  subnet {
+    name = "subnet2"
+    address_prefix = "10.0.2.0/24"
+  }
+
+  tags {
+    environment = "blue-world-finder"
+  }
+}
