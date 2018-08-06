1b21793a9a5697d0ecde76a00bc440cf4050cebf
sebug <sebastian.gfeller@gmail.com>
2018-08-06 12:37:04 +0200

    Splitting network, subnetwork

--- subnetworks.tf
+++ subnetworks.tf
@@ -1,22 +1,8 @@
-resource "google_compute_network" "our_development_network" {
-  name = "devnetwork"
-  auto_create_subnetworks = false
-}
-
 resource "google_compute_subnetwork" "dev-subnet" {
   ip_cidr_range = "10.0.1.0/24"
   name = "devsubnet"
   network = "${google_compute_network.our_development_network.self_link}"
   region = "europe-west2"
-}
-
-resource "aws_vpc" "environment-example-two" {
-  cidr_block = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support = true
-  tags {
-    Name = "terraform-aws-vpc-example-two"
-  }
 }
 
 resource "aws_subnet" "subnet1" {
@@ -44,32 +30,3 @@ resource "aws_security_group" "subnetsecurity" {
   }
 }
 
-resource "azurerm_resource_group" "azy_network" {
-  location = "West Europe"
-  name = "devresgrp"
-}
-
-resource "azurerm_virtual_network" "blue_virtual_network" {
-  address_space = ["10.0.0.0/16"]
-  location = "West Europe"
-  name = "bluevirtnetwork"
-  resource_group_name = "${azurerm_resource_group.azy_network.name}"
-  dns_servers = [
-    "10.0.0.4",
-    "10.0.0.5"
-  ]
-
-  subnet {
-    name = "subnet1"
-    address_prefix = "10.0.1.0/24"
-  }
-
-  subnet {
-    name = "subnet2"
-    address_prefix = "10.0.2.0/24"
-  }
-
-  tags {
-    environment = "blue-world-finder"
-  }
-}
